1183b4d1b207396b6abbc04aa91f8d69c4da817b
vozdvin <vozdvin@mail.ru>
2011-06-19 18:27:00 +0000

    add: Сделаны специальные страницы для HTTP-ошибок 404, 500 и 503.

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,8 +1,11 @@
 <div class="footer">
+  <div style="text-align:right;font-style:italic">
+    <span style="font-weight:bold">Beaver's&nbsp;Engine</span>&nbsp;&copy;&nbsp;by
+  </div>
   <div class="first">
-    &copy;&nbsp;Воздвиженский&nbsp;Илья&nbsp;aka&nbsp;<a href="mailto:vozdvin@mail.ru">Ключ</a>
+    Воздвиженский&nbsp;Илья&nbsp;aka&nbsp;<a href="mailto:vozdvin@mail.ru">Ключ</a>.
   </div>
   <div class="next">
-    Проект&nbsp;использует&nbsp;<a href="http://symfony-project.org">symfony</a>&nbsp;и&nbsp;<a href="http://www.php.net">PHP</a>
+    Проект&nbsp;использует&nbsp;<a href="http://symfony-project.org">symfony</a>&nbsp;и&nbsp;<a href="http://www.php.net">PHP</a>.
   </div>
 </div>
