e7e0dd188d124e4ac529c3aed97dff457fb0bd93
vozdvin@mail.ru <vozdvin@mail.ru@dd906366-b821-e8c9-b8bb-4119fb20b229>
2011-07-26 17:11:45 +0000

    fix: При работе с паролями при проверках на длину они могли быть отображены.

--- trunk/lib/form/AuthLoginForm.php
+++ trunk/lib/form/AuthLoginForm.php
@@ -16,17 +16,16 @@ class AuthLoginForm extends BaseForm
         'login' => new sfWidgetFormInputText(),
         'password' => new sfWidgetFormInputPassword()
     ));
-
     $this->setValidators(array(
         'login' => new sfValidatorString(array('min_length' => WebUser::MIN_NAME_LENGTH)),
-        'password' => new sfValidatorString(array('min_length' => WebUser::MIN_PWD_LENGTH))
+        'password' => new sfValidatorString(array('required' => true))
     ));
 
     //Configure
     $this->getWidgetSchema()->setNameFormat('auth[%s]');
     $this->getWidgetSchema()->setLabels(array(
-        'login' => 'Имя',
-        'password' => 'Пароль'
+        'login' => 'Имя:',
+        'password' => 'Пароль:'
     ));
   }
 
