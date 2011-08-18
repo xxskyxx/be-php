2d2b53895811083f67c569d4e89ef861926e72fd
vozdvin@mail.ru <vozdvin@mail.ru@dd906366-b821-e8c9-b8bb-4119fb20b229>
2011-08-18 15:37:09 +0000

    ref: Дополнения к предыдущей метке.

--- trunk/apps/frontend/templates/_header.php
+++ trunk/apps/frontend/templates/_header.php
@@ -1,7 +1,7 @@
 <div>
   <div style="text-align: left; float: left">
-    <div class="middleVertical"><img src="/images/favicon.png" alt="[BE]" onClick="document.location='/home/index'" /></div>
-    <div class="middleVertical"><span style="font-weight:bold"><?php echo SystemSettings::getInstance()->site_name ?></div>
+    <div style="display: inline-block; vertical-align: middle"><img src="/images/favicon.png" alt="[BE]" onClick="document.location='/home/index'" /></div>
+    <div style="display: inline-block; vertical-align: middle"><span style="font-weight:bold"><?php echo SystemSettings::getInstance()->site_name ?></div>
   </div>
   <div style="text-align: right; height: 1.5em">
     <?php
@@ -13,7 +13,7 @@
     else
     {
       echo link_to('Регистрация', 'auth/register');
-      echo ' |&nbsp;'.link_to('Вход', 'auth/login');
+      echo ' '.link_to('Вход', 'auth/login');
     }
     ?>
   </div>
