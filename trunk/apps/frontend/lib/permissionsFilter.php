17c8031f8b54f9f4ea075cfee06f8f13767c573c
vozdvin@mail.ru <vozdvin@mail.ru@dd906366-b821-e8c9-b8bb-4119fb20b229>
2011-08-21 14:08:51 +0000

    ref: авторизация

--- trunk/apps/frontend/lib/permissionsFilter.php
+++ trunk/apps/frontend/lib/permissionsFilter.php
@@ -8,16 +8,16 @@ class permissionsFilter extends sfFilter
     if ($this->isFirstCall())
     {
       $request = $this->getContext()->getRequest();
+      $moduleName = strtolower($request->getParameter('module'));
       //На эти модули у всех всегда есть право
-      if (strtoupper($request->getParameter('module')) != 'HOME'
-          && strtoupper($request->getParameter('module')) != 'AUTH')
+      if ( ! (($moduleName === 'home') || ($moduleName === 'auth')) )
       {
         $session = $this->getContext()->getUser();
         $controller = $this->getContext()->getController();
 
         if (!$session->isAuthenticated())
         {
-          $session->setFlash('error', 'Вы не авторизованы. Авторизуйтесь и повторите операцию.', false);
+          $session->setFlash('error', 'Вы не авторизованы. Авторизуйтесь и повторите операцию.');
           $controller->redirect('auth/login');
           exit; //Без этого действие все-равно выполнится.
         }
@@ -26,7 +26,7 @@ class permissionsFilter extends sfFilter
         $sessionWebUser = WebUser::byId($session->getAttribute('id', 0));
         if (!$sessionWebUser)
         {
-          $session->setFlash('error', 'Ваша учетная запись не найдена. Авторизуйтесь и повторите операцию.', false);
+          $session->setFlash('error', 'Ваша учетная запись не найдена. Авторизуйтесь и повторите операцию.');
           $controller->redirect('auth/logout');
           exit; //Без этого действие все-равно выполнится.
         }
@@ -34,7 +34,7 @@ class permissionsFilter extends sfFilter
         //Пользователь гарантированно есть и его id известен.
         if (!$sessionWebUser->is_enabled)
         {
-          $session->setFlash('error', 'Ваша учетная запись отключена, вы не можете делать что-либо.', false);
+          $session->setFlash('error', 'Ваша учетная запись отключена, вы не можете делать что-либо.');
           $controller->redirect('auth/activateManual');
           exit; //Без этого действие все-равно выполнится.
         }
