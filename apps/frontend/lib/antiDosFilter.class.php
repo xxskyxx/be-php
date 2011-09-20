5ebf1a8b5b0959b2483381e52376ede860a24474
vozdvin <vozdvin@mail.ru>
2011-09-20 16:32:02 +0000

    ref: Доработан фильтр, лимитирующий поток запросов в рамках сессии.

--- apps/frontend/lib/antiDosFilter.class.php
+++ apps/frontend/lib/antiDosFilter.class.php
@@ -1,31 +1,78 @@
 <?php
 
+/**
+ * Фильр, ограничивающий интенсивность потока запросов в рамках сессии.
+ */
 class antiDosFilter extends sfFilter
 {
-  
+  //Настройки ниже подобраны так, чтобы при локальном размещении сервера и браузера
+  //ручной генерацией запросов (по F5 в браузере) не удавалось вызвать срабатывание фильтра.
+  const INTEGRATION_INTERVAL = 7; //Интервал подсчета запросов, сек
+  const REQUESTS_TRESHOLD = 10; //Максимально допустимое число запросов за интервал
+  const BLOCK_INTERVAL = 30; //Интервал, на который блокируется доступ при превышении числа, сек
+
   public function execute($filterChain)
   {
     if ($this->isFirstCall())
     {
       $session = $this->getContext()->getUser();
+
       $timeNow = time();
-      $lastAccess = $session->getAttribute('lastAccess', 0);
-      $session->setAttribute('lastAccess', $timeNow);
-      $redirected = $session->getAttribute('redirected', 0);
-      if ($redirected > 0)
+      $lastTimeStamp = $session->getAttribute('lastTimeStamp', 0);
+      $requestCount = $session->getAttribute('requestCount', 1);
+      $blocked = $session->getAttribute('blocked', 0);
+
+      if ($lastTimeStamp == 0)
       {
-        $session->setAttribute('redirected', 0);
+        //Подсчет еще не начинался, приступим
+        $session->setAttribute('lastTimeStamp', $timeNow);
+        $session->setAttribute('requestCount', 1);
+        $session->setAttribute('blocked', 0);
       }
-      
-      if (($lastAccess == $timeNow) //т.е. разница между запросами меньше секунды.
-          && ($redirected == 0)) 
+      else
       {
-        include('../apps/frontend/config/error/unavailable.php');
-        exit;
+        if ($blocked == 0)
+        {
+          $requestCount = $requestCount + 1;
+          $session->setAttribute('requestCount', $requestCount);
+          
+          if (($timeNow - $lastTimeStamp) < antiDosFilter::INTEGRATION_INTERVAL)
+          {
+            //Интервал интегрирования еще не закончился
+            if ($requestCount >= antiDosFilter::REQUESTS_TRESHOLD)
+            {
+              //Превышено число запросов, блокируем
+              $session->setAttribute('lastTimeStamp', $timeNow);
+              $session->setAttribute('blocked', 1);
+            }
+          }
+          else
+          {
+            //Интервал интегрирования закончился, начнем новый.
+            $session->setAttribute('lastTimeStamp', $timeNow);
+            $session->setAttribute('requestCount', 1);
+          }
+        }
+        else
+        {
+          if ($lastTimeStamp + antiDosFilter::BLOCK_INTERVAL < $timeNow)
+          {
+            //Снимаем блокировку
+            $session->setAttribute('blocked', 0);
+            //Начинаем новый цикл счета
+            $session->setAttribute('lastTimeStamp', $timeNow);
+            $session->setAttribute('requestCount', 1);
+          }
+          else
+          {
+            include('../apps/frontend/config/error/unavailable.php');
+            exit;
+          }
+        }
       }
     }
     $filterChain->execute();
   }
-  
+
 }
 ?>
