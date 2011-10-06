dd934da1e491a39a96333411f09d4f5907778ebe
vozdvin <vozdvin@mail.ru>
2011-10-06 18:37:33 +0000

    ref: Улучшена работа защиты от слишком частых запросов; под подобную ситуацию сделана отдельная страница ошибки.

---
+++ apps/frontend/config/error/unavailable_antidos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="title" content="Beaver's Engine" />
+    <meta name="description" content="Beaver's Engine for interactive online games" />
+    <meta name="language" content="en" />
+    <meta name="robots" content="noindex, nofollow" />
+    <title>Beaver's Engine</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/common.css" />
+  </head>
+  <body>
+    <h1>
+      503... Service Unavailable
+    </h1>
+    <h3>- Ну и запросы у вас! - сказала база данных и повесилась.</h3>
+    <p>
+      Вы отправляете запросы слишком часто!
+    </p>
+    <p>
+      Будьте добры, подождите некоторое время и <a href="javascript:window.location.reload()">обновите страницу</a>.
+    </p>
+    <h3>Что дальше?</h3>
+    <ul>
+      <li style="margin-top:0.5em"><a href="javascript:window.location.reload()">Еще раз</a> попытаться</li>
+      <li><a href="javascript:history.go(-1)">Вернуться</a> на предыдущую страницу</li>
+      <li><a href="/home/index">Перейти на Главную</a> страницу сайта</li>
+    </ul>
+  </body>
+</html>
--- apps/frontend/lib/antiDosFilter.class.php
+++ apps/frontend/lib/antiDosFilter.class.php
@@ -5,9 +5,7 @@
  */
 class antiDosFilter extends sfFilter
 {
-  //Настройки ниже подобраны так, чтобы при локальном размещении сервера и браузера
-  //ручной генерацией запросов (по F5 в браузере) не удавалось вызвать срабатывание фильтра.
-  const INTEGRATION_INTERVAL = 7; //Интервал подсчета запросов, сек
+  const INTEGRATION_INTERVAL = 5; //Интервал подсчета запросов, сек
   const REQUESTS_TRESHOLD = 10; //Максимально допустимое число запросов за интервал
   const BLOCK_INTERVAL = 30; //Интервал, на который блокируется доступ при превышении числа, сек
 
@@ -18,56 +16,46 @@ class antiDosFilter extends sfFilter
       $session = $this->getContext()->getUser();
 
       $timeNow = time();
-      $lastTimeStamp = $session->getAttribute('lastTimeStamp', 0);
-      $requestCount = $session->getAttribute('requestCount', 1);
+      $lastTimeStamp = $session->getAttribute('lastTimeStamp', time());
+      $requestCount = $session->getAttribute('requestCount', 0);
       $blocked = $session->getAttribute('blocked', 0);
 
-      if ($lastTimeStamp == 0)
+      if ($blocked == 0)
       {
-        //Подсчет еще не начинался, приступим
-        $session->setAttribute('lastTimeStamp', $timeNow);
-        $session->setAttribute('requestCount', 1);
-        $session->setAttribute('blocked', 0);
-      }
-      else
-      {
-        if ($blocked == 0)
+        $requestCount = $requestCount + 1;
+        $session->setAttribute('requestCount', $requestCount);
+
+        if (($timeNow - $lastTimeStamp) < antiDosFilter::INTEGRATION_INTERVAL)
         {
-          $requestCount = $requestCount + 1;
-          $session->setAttribute('requestCount', $requestCount);
-          
-          if (($timeNow - $lastTimeStamp) < antiDosFilter::INTEGRATION_INTERVAL)
+          //Интервал интегрирования еще не закончился
+          if ($requestCount >= antiDosFilter::REQUESTS_TRESHOLD)
           {
-            //Интервал интегрирования еще не закончился
-            if ($requestCount >= antiDosFilter::REQUESTS_TRESHOLD)
-            {
-              //Превышено число запросов, блокируем
-              $session->setAttribute('lastTimeStamp', $timeNow);
-              $session->setAttribute('blocked', 1);
-            }
-          }
-          else
-          {
-            //Интервал интегрирования закончился, начнем новый.
+            //Превышено число запросов, блокируем
             $session->setAttribute('lastTimeStamp', $timeNow);
-            $session->setAttribute('requestCount', 1);
+            $session->setAttribute('blocked', 1);
           }
         }
         else
         {
-          if ($lastTimeStamp + antiDosFilter::BLOCK_INTERVAL < $timeNow)
-          {
-            //Снимаем блокировку
-            $session->setAttribute('blocked', 0);
-            //Начинаем новый цикл счета
-            $session->setAttribute('lastTimeStamp', $timeNow);
-            $session->setAttribute('requestCount', 1);
-          }
-          else
-          {
-            include('../apps/frontend/config/error/unavailable.php');
-            exit;
-          }
+          //Интервал интегрирования закончился, начнем новый.
+          $session->setAttribute('lastTimeStamp', $timeNow);
+          $session->setAttribute('requestCount', 0);
+        }
+      }
+      else
+      {
+        if ($lastTimeStamp + antiDosFilter::BLOCK_INTERVAL < $timeNow)
+        {
+          //Снимаем блокировку
+          $session->setAttribute('blocked', 0);
+          //Начинаем новый цикл счета
+          $session->setAttribute('lastTimeStamp', $timeNow);
+          $session->setAttribute('requestCount', 0);
+        }
+        else
+        {
+          include('../apps/frontend/config/error/unavailable_antidos.php');
+          exit;
         }
       }
     }
