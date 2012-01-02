601a09a9ebed63b161a084109edccfde92065715
vozdvin <vozdvin@mail.ru>
2012-01-02 19:27:55 +0000

    fix: Переход на исходную страницу при выборе региона ссылкой из заголовка.

--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -3,12 +3,11 @@
     <?php
     if ($sf_user->isAuthenticated())
     {
-      //$backLink = 'home/index';
-      $backLink = $_SERVER['PHP_SELF'].$_SERVER['QUERY_STRING'];
+      $backLinkEncoded = Utils::encodeSafeUrl($_SERVER['REQUEST_URI']);
       echo '(';
       echo link_to($sf_user->getAttribute('login'), 'webUser/show?id='.$sf_user->getAttribute('id'));
       echo '&nbsp;@&nbsp;';
-      echo link_to(Region::byId($sf_user->getAttribute('region_id'))->name, 'region/setCurrent?returl='.Utils::encodeSafeUrl($backLink));
+      echo link_to(Region::byId($sf_user->getAttribute('region_id'))->name, 'region/setCurrent?returl='.$backLinkEncoded);
       echo ') '.link_to('Выйти', 'auth/logout');
     }
     else
