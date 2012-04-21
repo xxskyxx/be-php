685ccef733e4b6fce5819e7a10b7c8d3e4a71a64
vozdvin <vozdvin@mail.ru>
2012-04-21 15:35:06 +0000

    add: В подвалах страниц сайта и на странице текущего задания добавлены часы с текущим временем (работает только в браузерах с JavaScript).

--- apps/frontend/templates/_metas.php
+++ apps/frontend/templates/_metas.php
@@ -5,4 +5,16 @@
 <link rel="shortcut icon" href="/customization/images/favicon.png" />
 <?php include_stylesheets() ?>
 <?php include_javascripts() ?>
+<script type="text/javascript">
+  function startTime() {
+    var time = new Date();
+    var h = time.getHours();
+    var m = time.getMinutes();
+    var s = time.getSeconds();
+    if (m < 10) { m = "0" + m };
+    if (s < 10) { s = "0" + s };
+    document.getElementById('serverTime').innerHTML = h + ":" + m + ":" + s;
+    html = setTimeout('startTime()',500);
+  }
+</script>
 </head>
--- customization/footer.php
+++ customization/footer.php
@@ -12,12 +12,14 @@
 ?>
 
 <div style="width:100%; border: none; padding: 0 0 0 0; margin: 0 0 0 0"> 
-  <div style="width:70%; display:inline-block;">
+  <div style="width:30%; display:inline-block; font-size: smaller">
+    Серверное время: <span id="serverTime">--:--:--</span>.
+  </div><div style="width:40%; display:inline-block;">
 
     <!-- Коды информеров и прочих баннеров -->
 
-  </div><div style="width:30%; display:inline-block;">
-    <span style="text-align: right; font-style: italic; font-size: smaller">&copy;&nbsp;Илья <?php echo mail_to(SystemSettings::getInstance()->contact_email_addr, 'Ключ')?> Воздвиженский</span>
+  </div><div style="width:30%; display:inline-block; text-align: right;">
+    <span style="font-style: italic; font-size: smaller">&copy;&nbsp;Илья <?php echo mail_to(SystemSettings::getInstance()->contact_email_addr, 'Ключ')?> Воздвиженский</span>
   </div>
 </div>
 
